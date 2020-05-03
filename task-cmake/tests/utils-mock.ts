556e792801b7ac000bbe4dddcffa48f0ad75df12
luca <681992+lukka@users.noreply.github.com>
2020-05-03 13:40:08 -0700

    absorb "add github action matchers" support and fix test's mocks.

--- task-cmake/tests/utils-mock.ts
+++ task-cmake/tests/utils-mock.ts
@@ -2,13 +2,27 @@
 // Released under the term specified in file LICENSE.txt
 // SPDX short identifier: MIT
 
-import * as os from 'os';
+export class Matcher {
+    constructor(private name: string, private fromPath?: string) {
+    }
+
+    dispose(): void {
+        // Intentionally void
+    }
+}
 
 export const utilsMock: any = {
-  wrapOpSync(name: string, fn: () => any): any {
+    wrapOpSync(name: string, fn: () => any): any {
         return fn();
     },
     wrapOp(name: string, fn: () => any): any {
         return fn();
+    },
+    createMatcher(name: string, fromPath?: string): any {
+        return {
+            dispose: function (): void {
+                // Intentionally void.
+            }
+        };
     }
 };
